1eef843ba60c42cd9da2327ff185480d92f0fde8
stephen <tasb00429@gmail.com>
2020-02-09 03:17:26 +0800

    [ Add ]：TableView and CollectionView Extension, [ Update ]：Build schema

--- Aletheia/Classes/ExtensionsUI/Extension+UICollectionView.swift
+++ Aletheia/Classes/ExtensionsUI/Extension+UICollectionView.swift
@@ -12,7 +12,15 @@ import UIKit
 /// so 'UITableView' can benefit from it
 extension AletheiaWrapper where Base == UICollectionView {
     
+    public func register<T: UICollectionViewCell>(_: T.Type) {
+        base.register(T.self, forCellWithReuseIdentifier: String(describing: T.self))
+    }
     
+    public func dequeueReusableCell<T: UICollectionViewCell>(forIndexPath indexPath: IndexPath) -> T {
+        guard let cell = base.dequeueReusableCell(withReuseIdentifier: String(describing: T.self), for: indexPath) as? T else {
+            fatalError("Could not dequeue cell with identifier: \(String(describing: T.self))")
+        }
+        
+        return cell
+    }
 }
-
-
--- Aletheia/Classes/ExtensionsUI/Extension+UITableView.swift
+++ Aletheia/Classes/ExtensionsUI/Extension+UITableView.swift
@@ -10,11 +10,29 @@ import UIKit
 /// UIView haa been conform to protocol 'AletheiaCompatible',
 /// so 'UITableView' can benefit from it
 extension AletheiaWrapper where Base == UITableView {
-    
-    
+
+    public func register<T: UITableViewCell>(_: T.Type) {
+        base.register(T.self, forCellReuseIdentifier: String(describing: T.self))
+    }
+
+    public func dequeueReusableCell<T: UITableViewCell>(forIndexPath indexPath: IndexPath) -> T {
+        guard let cell = base.dequeueReusableCell(withIdentifier: String(describing: T.self), for: indexPath) as? T else {
+            fatalError("Could not dequeue cell with identifier: \(String(describing: T.self))")
+        }
+        
+        return cell
+    }
+
+    public func registerHeaderFooter<T: UITableViewHeaderFooterView>(_: T.Type) {
+        base.register(T.self, forHeaderFooterViewReuseIdentifier: String(describing: T.self))
+    }
+
+    public func dequeueReusableHeaderFooterView<T: UITableViewHeaderFooterView>() -> T {
+        guard let headerFooterView = base.dequeueReusableHeaderFooterView(withIdentifier: String(describing: T.self)) as? T else {
+            fatalError("Could not dequeue header footer view with identifier: \(String(describing: T.self))")
+        }
+        
+        return headerFooterView
+    }
 }
 
-extension AletheiaWrapper where Base == UITableViewCell {
-    
-    
-}
